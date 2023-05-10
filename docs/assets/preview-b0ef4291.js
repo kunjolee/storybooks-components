c48c5dc86de0edfaa9a022a402bf928aea77310e
kunjolee28 <Kunjo.JosueLee@telus.com>
2023-05-09 23:22:11 -0600

    fix: change docs

---
+++ docs/assets/preview-b0ef4291.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./iframe-52df345a.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-EYKKDMVH-b4e2b8e3.js"),["./DocsRenderer-EYKKDMVH-b4e2b8e3.js","./iframe-52df345a.js","./index-8db94870.js","./_commonjsHelpers-042e6b4d.js","./react-18-5792738b.js","./index-9900c05c.js","./index-884b050f.js","./index-d475d2ea.js","./index-d37d4223.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
+//# sourceMappingURL=preview-b0ef4291.js.map
